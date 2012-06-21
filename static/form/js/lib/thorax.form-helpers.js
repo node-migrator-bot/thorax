9e9c0a4a8cc6afbcd2867431cf0ce089632cd56b
Ryan Eastridge <ryan@syntacticx.com>
2012-06-21 16:53:49 -0700

    control-group form helper

--- static/form/js/lib/thorax.form-helpers.js
+++ static/form/js/lib/thorax.form-helpers.js
@@ -1,56 +1,103 @@
 /*
 
-  
-{{#error}}
-  error
-{{/error}}
+TODO:
 
-{{input-error for="id"}}
+- input
+- error
+- attribute-error
+- label
+- control-group
 
+{{#control-group}}
+  {{label}}
+  {{attribute-error}}
+  {{input}}
+{{/control-group}}
+
+
+FormView = new Application.View
+  events:
+    'submit form': (event) ->
+      @serialize event, (attributes, release) ->
+        @$('form').append '<div></div>'
+  template: """
+    <form class="form-vertical">
+      {{#error}}{{errors}}{{/error}}
+      <fieldset>
+        {{}}
+        <div class="form-actions">
+          <button type="submit" class="btn btn-primary">Save changes</button>
+          <button class="btn">Cancel</button>
+        </div>
+      </fieldset>
+    </form>
+  """
 
 */
+
 var errorClassName = 'error',
     errorAttributeName = 'data-view-error',
     inputErrorAttributeName = 'data-input-error-id';
 
 Thorax.View.registerHelper('error', function(options) {
-  var tag = options.hash.tag || options.hash.tagName || 'div';
-  delete options.hash.tag;
-  delete options.hash.tagName;
+  var partialId = _.uniqueId('error-');
+  this._view[partialId] = function(options) {
+    return options.fn(_.extend({}, this, {
+      errors: options.hash.errors
+    }));
+  };
   options.hash['class'] = options.hash['class'] || 'alert alert-error',
   options.hash[errorAttributeName] = 'true';
-  return new Handlebars.SafeString(View.tag(options.hash, options.fn(this)));
+  return Handlebars.helpers.partial.call(this, partialId, {
+    fn: options.fn,
+    hash: _.extend({
+      anonymous: true
+    }, options.hash)
+  });
 });
 
-Thorax.View.registerHelper('attribute-error', function(forInputId, options) {
-  var tag = options.hash.tag || options.hash.tagName || 'span';
-  delete options.hash.tag;
-  delete options.hash.tagName;
+Thorax.View.registerHelper('input-error', function(forInputId, options) {
   options.hash['class'] = options.hash['class'] || 'help-inline';
   options.hash[inputErrorAttributeName] = forInputId;
-  return new Handlebars.SafeString(View.tag(options.hash));
+  return new Handlebars.SafeString(Thorax.View.tag(options.hash));
 });
 
 Thorax.View.registerHelper('input', function(options) {
   var tag = 'input',
       content = null,
-      output = '';
-  options.hash.id = options.hash.id
-    ? Thorax.View.expandToken(options.hash.id, this)
-    : (options.hash.name || _.uniqueId('input')) + '-' + this.cid;
+      inputGenerator,
+      labelGenerator,
+      inputErrorMessageGenerator,
+      id;
+  id = options.hash.id = options.hash.id || ((options.hash.name || _.uniqueId('input')) + '-' + this.cid);
   options.hash.placeholder = options.hash.placeholder || options.hash.label;
+  
   if (options.hash.label) {
-    output += Thorax.View.tag({
-      tag: 'label',
-      'for': options.hash.id,
-      'class': options.hash['label-class'] || 'control-label'
-    });
-    delete options.hash['label-class'];
+    labelGenerator = function(generatorOptions) {
+      return Thorax.View.tag(_.extend({
+        tag: 'label',
+        'for': options.hash.id,
+        'class': 'control-label'
+      }, generatorOptions ? generatorOptions.hash : {}));
+    };
     delete options.hash.label;
   }
-  if (options.fn) {
-    output += options.fn(this);
+
+  if (!('error-message' in options.hash) || options.hash['error-message']) {
+    if (options.hash['error-message']) {
+      options.hash['data-error-message'] = options.hash['error-message'];
+      delete options.hash['error-message'];
+    }
+    inputErrorMessageGenerator = function(generatorOptions) {
+      var inputErrorMessageOptions = {
+        tag: 'p',
+        'class': 'help-block',
+      };
+      inputErrorMessageOptions[inputErrorAttributeName] = id;
+      return Thorax.View.tag(_.extend(inputErrorMessageOptions, generatorOptions ? generatorOptions.hash : {}));
+    };
   }
+
   if (options.hash.type === 'textarea') {
     content = '';
     tag = 'textarea';
@@ -62,11 +109,11 @@ Thorax.View.registerHelper('input', function(options) {
         return {
           value: value,
           label: key,
-          selected: false
+          selected: value == options.hash.selected
         }
       }, this);
     }
-    content = _.map(function(option) {
+    content = _.map(options, function(option) {
       var tagOptions = {
         tag: 'option',
         value: options.value
@@ -74,38 +121,54 @@ Thorax.View.registerHelper('input', function(options) {
       if (option.selected) {
         tagOptions.selected = selected;
       }
-      return Thorax.View.tag(tagOptions, option.label);
-    }).join('');
+      return Thorax.View.tag(tagOptions, option.label, this);
+    }, this).join('');
     delete options.hash.type;
     delete options.hash.options;
+    delete options.hash.selected;
   }
-  output += Thorax.View.tag(options.hash, content);
-  return new Handlebars.SafeString(output);
+  inputGenerator = function(generatorOptions) {
+    return Thorax.View.tag(_.extend({}, options.hash, generatorOptions ? generatorOptions.hash : {}), content, this);
+  };
+
+  var returnObjects = options.hash['return-objects'];
+  delete options.hash['return-objects'];
+
+  if (returnObjects) {
+    return {
+      input: function() {
+        return new Handlebars.SafeString(inputGenerator.apply(this, arguments));
+      },
+      label: function() {
+        return new Handlebars.SafeString(labelGenerator.apply(this, arguments));
+      },
+      'input-error': function() {
+        return new Handlebars.SafeString(inputErrorMessageGenerator.apply(this, arguments));
+      }
+    };
+  } else {
+    return new Handlebars.SafeString(labelGenerator.call(this) + inputErrorMessageGenerator.call(this) + inputGenerator.call(this));
+  }
 });
-//
-//Thorax.View.registerHelper('label', function() {
-//
-//});
-//
-//Thorax.View.registerHelper('control-group', function() {
-//
-//});
 
-function attributeError() {
+Thorax.View.registerHelper('control-group', function(options) {
+  var generators = Handlebars.helpers.input({
+    hash: {
+      'return-objects': true,
+      name: options.hash.name,
+      label: options.hash.label
+    }
+  });
+  delete options.hash.name;
+  delete options.hash.label;
+  var context = _.extend({}, this, {
+    input: generators.input,
+    label: generators.label,
+    'input-error': generators['input-error']
+  });
+  return new Handlebars.SafeString(Thorax.View.tag(options.hash, options.fn(context), this));
+});
 
-}
-
-//  options.hash.type = options.hash.type || 'text';
-//  options.hash.id = options.hash.id ? expandToken(options.hash.id, scope) : _.uniqueId('txt');
-
-//Handlebars.escapeExpression
-//Thorax.View.reigsterHelper('control-group', function(options) {
-//  var id = 'input-' + this.cid,
-//      label = options.hash.label,
-//      
-//
-//
-//});
 
 /*
 
@@ -126,10 +189,10 @@ function attributeError() {
 //      </div>
 //    </div>
 //
-/*
+
 function resetErrorState() {
-  this.$('[' + errorAttributeName ']').hide();
-  this.$('[' + inputErrorAttributeName + ']').html().hide();
+  this.$('[' + errorAttributeName + ']').empty().hide();
+  this.$('[' + inputErrorAttributeName + ']').empty().hide();
   this.$('.control-group.' + errorClassName).removeClass(errorClassName);
 }
 
@@ -137,11 +200,21 @@ Thorax.View.registerEvents({
   rendered: resetErrorState,
   serialize: resetErrorState,
   error: function(errors) {
-    this.$('[' + errorAttributeName ']').show();
+    var $error = this.$('[' + errorAttributeName + ']');
+    _.each($error, function(el) {
+      var partialName = $(el).attr('data-partial-name');
+      this[partialName]({
+        errors: errors
+      });
+    }, this);
+    $error.show();
     errors.forEach(function(error) {
-      this.$('[' + errorAttributeName '="' + error.id + '"]']).html(error.message).show();
-      error.element.closest('.control-group').addClass(errorClassName);
+      var errorAttributeEl = this.$('[' + inputErrorAttributeName + '="' + error.id + '"]');
+      errorAttributeEl.html(error.message);
+      errorAttributeEl.show();
+      if (error && error.element) {
+        error.element.closest('.control-group').addClass(errorClassName);
+      }
     }, this);
   }
 });
-*/
